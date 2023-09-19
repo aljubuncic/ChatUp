5fb586589e6ebce67339a22411984948a5c2a0c3
esma.zejnilovic <esma.zejnilovic@zira.com.ba>
2023-09-19 22:52:15 +0200

    fixes

--- app/src/main/java/ba/etf/chatapp/SignUpActivity.kt
+++ app/src/main/java/ba/etf/chatapp/SignUpActivity.kt
@@ -4,7 +4,6 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
@@ -99,7 +98,6 @@ class SignUpActivity : AppCompatActivity() {
                                 override fun onDataChange(dataSnapshot: DataSnapshot) {
                                     var exist = false
                                     for(snapshot in dataSnapshot.children) {
-                                        Log.i("xxx", "xxx")
                                         exist = true
                                         if (!snapshot.getValue(User::class.java)!!.parent) {
                                             progressDialog.dismiss()
